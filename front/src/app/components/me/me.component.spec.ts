99e1dd342a8d9a9dabc66bc8f648782da94075f7
francoismk <francois.szymczak@gmail.com>
2025-03-14 20:19:59 +0100

    feat: add unit test of me component

--- front/src/app/components/me/me.component.spec.ts
+++ front/src/app/components/me/me.component.spec.ts
@@ -8,10 +8,24 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { SessionService } from 'src/app/services/session.service';
 
 import { MeComponent } from './me.component';
+import {UserService} from "../../services/user.service";
+
+import { of } from 'rxjs';
+import { expect } from '@jest/globals';
 
 describe('MeComponent', () => {
   let component: MeComponent;
   let fixture: ComponentFixture<MeComponent>;
+
+  const mockUser = {
+    id: 1,
+    firstName: 'Jean',
+    lastName: 'Dupont',
+    email: 'jean.dupont@test.com',
+    admin: true,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  }
 
   const mockSessionService = {
     sessionInformation: {
@@ -19,6 +33,12 @@ describe('MeComponent', () => {
       id: 1
     }
   }
+
+  const mockUserService = {
+    getById: jest.fn(() => of(mockUser)),
+    delete: jest.fn(() => of({})),
+  }
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [MeComponent],
@@ -30,7 +50,8 @@ describe('MeComponent', () => {
         MatIconModule,
         MatInputModule
       ],
-      providers: [{ provide: SessionService, useValue: mockSessionService }],
+      providers: [{ provide: SessionService, useValue: mockSessionService },
+        {provide: UserService, useValue: mockUserService}],
     })
       .compileComponents();
 
@@ -42,4 +63,15 @@ describe('MeComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should fetch user data on component init', () => {
+    expect(mockUserService.getById).toHaveBeenCalledWith('1');
+    expect(component.user).toEqual(mockUser);
+  })
+
+  it('should not display delete button if user is admin', () => {
+    const deleteButton = fixture.nativeElement.querySelector('button[color="warn"]');
+    expect(mockSessionService.sessionInformation).toEqual({admin: true, id: 1});
+    expect(deleteButton).toBeNull();
+  })
 });
