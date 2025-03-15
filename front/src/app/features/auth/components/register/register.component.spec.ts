9e9323a3a5ed0889f4df0e703b4afb352e81ffd8
francoismk <francois.szymczak@gmail.com>
2025-03-15 12:24:54 +0100

    feat: add unit test of register component

--- front/src/app/features/auth/components/register/register.component.spec.ts
+++ front/src/app/features/auth/components/register/register.component.spec.ts
@@ -9,18 +9,33 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { expect } from '@jest/globals';
 
 import { RegisterComponent } from './register.component';
+import {AuthService} from "../../services/auth.service";
+import {of} from "rxjs";
+import {Router} from "@angular/router";
+
+const mockAuthService = {
+  register: jest.fn()
+};
+
+const mockRouter = {
+  navigate: jest.fn()
+};
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
+  let authService: AuthService;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [RegisterComponent],
+      providers: [   { provide: AuthService, useValue: mockAuthService },
+        { provide: Router, useValue: mockRouter }],
       imports: [
         BrowserAnimationsModule,
         HttpClientModule,
-        ReactiveFormsModule,  
+        ReactiveFormsModule,
         MatCardModule,
         MatFormFieldModule,
         MatIconModule,
@@ -31,10 +46,59 @@ describe('RegisterComponent', () => {
 
     fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
+    authService = TestBed.inject(AuthService)
+    router = TestBed.inject(Router)
     fixture.detectChanges();
   });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  })
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should invalid form when empty', () => {
+    expect(component.form.invalid).toBeTruthy();
+  });
+
+  it ('should invalid form when email is invalid', () => {
+    component.form.controls.email.setValue('test');
+    expect(component.form.valid).toBeFalsy();
+  });
+
+  it('should invalid form when password is invalid', () => {
+    component.form.controls.password.setValue('te');
+    expect(component.form.valid).toBeFalsy();
+  });
+
+  it('should invalid form when firstName is invalid', () => {
+    component.form.controls.firstName.setValue('');
+    expect(component.form.valid).toBeFalsy();
+  })
+
+  it('should invalid form when lastName is invalid', () => {
+    component.form.controls.lastName.setValue('');
+    expect(component.form.valid).toBeFalsy();
+  });
+
+  it('should register a user when form is valid', () => {
+    const registerData = {
+      email: 'jean.dupont@test.fr',
+      firstName: 'Jean',
+      lastName: 'Dupont',
+      password: 'password',
+    }
+
+    mockAuthService.register.mockReturnValueOnce(of(registerData));
+
+    component.form.setValue(registerData);
+    component.submit();
+
+    expect(component.form.valid).toBeTruthy();
+    expect(authService.register).toHaveBeenCalledWith(registerData);
+    expect(router.navigate).toHaveBeenCalledWith(['/login']);
+  })
+
 });
